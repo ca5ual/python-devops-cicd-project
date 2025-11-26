ae8b35a8e8652edeaee3b6d95ab08a1251d67914
Denis Zhak <deniszhak4@gmail.com>
2025-11-26 19:31:44 +0100

    fix linting

--- src/simple_http_checker/checker.py
+++ src/simple_http_checker/checker.py
@@ -1,30 +1,31 @@
-import logging 
+import logging
 import requests
 
 logger = logging.getLogger(__name__)
 
+
 def check_urls(urls: list[str], timeout: int = 5) -> dict[str, str]:
     """
     Checks a list of URLs and returns their status.
-    
+
     Args:
         urls: A list of URL strings to check.
         timeout: Maximum time in seconds to wait for each requests. Default to 5
-        
+
     Returns:
         A dictionary mapping each URL to its status string.
     """
-    logger.info (f"Starting check for {len(urls)} URLs with a timeout of {timeout}")
-    
-    results: dict [str,str] = {}
-    
+    logger.info(f"Starting check for {len(urls)} URLs with a timeout of {timeout}")
+
+    results: dict[str, str] = {}
+
     for url in urls:
         status = "UNKNOWN"
-        
+
         try:
             logger.debug(f"Checking URL: {url}")
             response = requests.get(url, timeout=timeout)
-            
+
             if response.ok:
                 status = f"{response.status_code} OK"
             else:
@@ -37,11 +38,12 @@ def check_urls(urls: list[str], timeout: int = 5) -> dict[str, str]:
             logger.warning(f"Connection error for {url}.")
         except requests.exceptions.RequestException as e:
             status = f"REQUEST_ERROR: {type(e).__name__}"
-            logger.error(f"An unexpected requests error occured for {url}: {e}",
-                         exc_info=True)
-            
+            logger.error(
+                f"An unexpected requests error occured for {url}: {e}", exc_info=True
+            )
+
         results[url] = status
         logger.debug(f"Checked: {url:<40} -> {status}")
-    
+
     logger.info("URL check finished.")
     return results
